c21ddbd627a4184b8fe9cd22144eb0e140954f38
Enirdas <enirdas@gmail.com>
2019-02-19 14:35:31 -0500

    Initial commit

---
+++ src/components/cercle.vue
@@ -0,0 +1,6 @@
+<template>
+ <div class="image">
+ <svg height="100" width="60">
+  <circle cx="30" cy="30" r="20" stroke="rgb(8, 93, 142)" stroke-width="3" fill="rgb(8, 93, 142)" /> 
+  </svg> 
+  </div>
